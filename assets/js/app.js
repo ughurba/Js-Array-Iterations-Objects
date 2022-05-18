049ac36490949664739c98009967652516ee553d
ughurba <ughurba@edu.code.az>
2022-05-19 00:27:21 +0400

    I completed the first task

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// 2) Ededlerden ibaret bir array verilir. Hemin arrayi boyukden ededden kiciye  dogru sort elemek.
+
+let numberArr= [2,3,10,44,23,12]
+
+
+console.log(numberArr.sort((a,b) => a-b ).reverse())
+ 
