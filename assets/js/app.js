4dfd1a7d23d86fbef68bdae7bbace41e75455ff4
ughurba <ughurba@edu.code.az>
2022-05-19 02:03:49 +0400

    refactor:finished tasks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,35 @@
 // 2) Ededlerden ibaret bir array verilir. Hemin arrayi boyukden ededden kiciye  dogru sort elemek.
 
-// let numberArr= [2,3,10,44,23,12]
+let numberArr = [2, 3, 10, 44, 23, 12];
 
-// console.log(numberArr.sort((a,b) => a-b).reverse())
- 
-
+console.log(numberArr.sort((a, b) => a - b).reverse());
 
 // 3) ["Yanvar","Fevral","Mart","Aprel"]  - bu arrayi parametr kimi qebul eden bir function yazirsiz.Hemin function geriye yeni bir array qaytarir.
 // Yeni yaranan arrayda hemin aylarin necenci ay oldugu yazilmalidir.
-// Cavab bele olmlalidir: ["Yanvar -1","Fevral-2","Mart-3","Aprel-4"]. 
+// Cavab bele olmlalidir: ["Yanvar -1","Fevral-2","Mart-3","Aprel-4"].
 
+function month(arr) {
+  let num = 0;
 
-
-function month(arr){
-    let num=0;
-    
-    const new_array = arr.map(value => {
-        ++num;
-        value+= -num
-        return value
-    })
-    return new_array
+  const new_array = arr.map((value) => {
+    ++num;
+    value += -num;
+    return value;
+  });
+  return new_array;
 }
 
- console.log( month(["Yanvar","Fevral","Mart","Aprel","May","Iyun"]))
+console.log(month(["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun"]));
 
-
-//  4)String qebul eden bir function yaziriq. Hemin function yeni bir array qaytaracaq, hemin arrayin elementleri 
+//  4)String qebul eden bir function yaziriq. Hemin function yeni bir array qaytaracaq, hemin arrayin elementleri
 //functiona gelen stringin sozlerinin herflerinin cemine beraber olmalidir.
 //  Meselen: Functiona gelen string - "Salam olsun hamiya", hemin functionun qaytardiqi array ise - [5,5,6] olacaq.
 
-function acceptString(str){
-    
+function acceptString(str) {
+  let arrayOfStrings = str.split(" ");
+
+  const lengthArr = arrayOfStrings.map((value) => value.length);
+
+  return lengthArr;
 }
+console.log(acceptString("salam olsun hamiya"));
